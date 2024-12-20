6a5ab7b73b66eaa821241945d20059f73d382508
emmyupdates <63416713+emmyupdates@users.noreply.github.com>
2024-12-20 16:57:36 +0000

    Imaginary guest invites, using pop method and f strings

---
+++ guest_list.py
@@ -0,0 +1,10 @@
+guest_list = ['bart nickerson', 'ashley lyle', 'jonathan lisco']
+bart = guest_list.pop(0)
+jonathan = guest_list.pop(1)
+ashley = guest_list.pop()
+
+print(f"You have been invited to speak at Manchester Comic Con 2025!")
+print(f"{bart.title()}, we need to know what to expect in Yellowjackets Season 4")
+print(f"And {ashley.title()}, you've gotta tell us why Shauna did that one thing in Season 3")
+print(f"We hope you can make it, {jonathan.title()}! We've dying to know how you came up with Episode 3 in Season 3")
+
