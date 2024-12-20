3ec255e017bbf04018db2e062a5e1d9c108b1dee
emmyupdates <63416713+emmyupdates@users.noreply.github.com>
2024-12-20 22:24:34 +0000

    Multiple ways for changing the order of lists

--- 03_chapter_three/seeing_the_world.py
+++ 03_chapter_three/seeing_the_world.py
@@ -1,4 +1,27 @@
 places = ['california', 'iceland', 'japan', 'new zealand', 'new jersey']
+print("This is the original order:")
 print(places)
 
+print("This is alphabetical order:")
 print(sorted(places))
+
+print("That wasn't permanant:")
+print(places)
+
+print("But this is:")
+places.reverse()
+print(places)
+
+print("Just kidding!")
+places.reverse()
+print(places)
+
+print("You can put it in alphabetical order permanantly")
+places.sort()
+print(places)
+
+print("I didn't say which way!")
+places.sort(reverse=True)
+print(places)
+
+print("And that's how you change the order of lists!")
