462417b00c49d8d0336a646203665a21885b1a53
emmyupdates <63416713+emmyupdates@users.noreply.github.com>
2024-12-20 17:37:04 +0000

    Modifying a guest

--- guest_list.py
+++ guest_list.py
@@ -1,10 +1,17 @@
 guest_list = ['bart nickerson', 'ashley lyle', 'jonathan lisco']
+print(guest_list)
+
 bart = guest_list.pop(0)
 jonathan = guest_list.pop(1)
 ashley = guest_list.pop()
 
-print(f"You have been invited to speak at Manchester Comic Con 2025!")
-print(f"{bart.title()}, we need to know what to expect in Yellowjackets Season 4")
-print(f"And {ashley.title()}, you've gotta tell us why Shauna did that one thing in Season 3")
-print(f"We hope you can make it, {jonathan.title()}! We've dying to know how you came up with Episode 3 in Season 3")
+print(f"This is a guest panel announcement for Manchester Comic Con 2025!")
+print(f"{bart.title()} will tell us what to expect in Yellowjackets Season 4,")
+print(f"You can ask {ashley.title()} about the Season 3 finale")
+print(f"And {jonathan.title()} will explain why Shauna did that one thing!")
+print(f"Due to schedule clashes, {jonathan.title()} will no longer be on the panel.")
 
+guest_list = ['bart nickerson', 'ashley lyle', 'jonathan lisco']
+guest_list[2] = 'melanie lynskey'
+print(guest_list)
+print("Good news! Melanie Lynskey will now be joining us")
