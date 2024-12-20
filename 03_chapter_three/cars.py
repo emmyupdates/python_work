b8e48ac1d98e350021747ca351029415e74dea12
emmyupdates <63416713+emmyupdates@users.noreply.github.com>
2024-12-20 22:13:27 +0000

    Updated one project, created a new one - still about sorting

--- 03_chapter_three/cars.py
+++ 03_chapter_three/cars.py
@@ -1,5 +1,2 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru']
-print(cars)
-
-cars.reverse()
-print(cars)
+print(len(cars))
---
+++ 03_chapter_three/seeing_the_world.py
@@ -0,0 +1,4 @@
+places = ['california', 'iceland', 'japan', 'new zealand', 'new jersey']
+print(places)
+
+print(sorted(places))
