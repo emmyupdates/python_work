334e5d751235ca3e4070d7995395cfd84ea20700
emmyupdates <63416713+emmyupdates@users.noreply.github.com>
2024-12-22 21:45:49 +0000

    Remembering how to use for loops from memory

---
+++ 04_chapter_four/pizza.py
@@ -0,0 +1,8 @@
+#Five Papa John's pizzas
+pizzas = ['margarita', 'pepperoni', 'all the meats', 'garden party', 'the works']
+print("Please take a look at our menu.")
+for pizza in pizzas:
+    print(f"How about our {pizza.title()}?")
+
+print("Thank you for ordering at Papa John's")
+print("We hope to see you again soon!")
