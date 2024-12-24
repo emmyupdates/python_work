e00d842818b8829de77a7a230f73fe0cf632820e
emmyupdates <63416713+emmyupdates@users.noreply.github.com>
2024-12-24 22:18:59 +0000

    Using range function with a for loop / listing numbers with the range and list functions

--- 04_chapter_four/first_numbers.py
+++ 04_chapter_four/first_numbers.py
@@ -1,2 +1,7 @@
-for value in range(1, 5):
+#Using the range function and a for loop to generate a series of numbers
+for value in range(1, 6):
     print(value)
+
+#Another way to create a list
+numbers = list(range(1, 6))
+print(numbers)
