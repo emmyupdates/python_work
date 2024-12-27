6d953d5a5ac938eff1519af630e8b9d91438745b
emmyupdates <63416713+emmyupdates@users.noreply.github.com>
2024-12-27 21:07:38 +0000

    Using the min, max and sum functions

---
+++ 04_chapter_four/simple_statistics.py
@@ -0,0 +1,11 @@
+#Creating the list
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+#Selects smallest number
+min(digits)
+
+#Selects biggest number
+max(digits)
+
+#Adds all numbers together
+sum(digits)
--- 04_chapter_four/square_numbers.py
+++ 04_chapter_four/square_numbers.py
@@ -1,3 +1,4 @@
+#List prints square numbers from 1 to 10
 squares = []
 for value in range(1, 11):
     square = value ** 2
