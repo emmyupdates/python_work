c447506f75f25ccc8a3f1a4ec8238b1fe789dae3
emmyupdates <63416713+emmyupdates@users.noreply.github.com>
2024-12-28 23:03:24 +0000

    List-loop to a million, as well as using min, max and sum functions

--- 04_chapter_four/million.py
+++ 04_chapter_four/million.py
@@ -1,3 +1,10 @@
 #Make a list of the numbers from one to one million, and then use a for loop to print the numbers.
-for million in list(range(1, 1_000_001)):
-    print(million)
+#for million in list(range(1, 1_000_001)):
+    #print(million)
+
+#Make sure your list actually starts at one and ends at one million
+#Also, see how quickly Python can add a million numbers
+millions = range(1, 1_000_001)
+print(min(millions))
+print(max(millions))
+print(sum(millions))
